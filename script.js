dcfa3afea52760fb5ebc267ab338ef487bd7b472
Sanaual <sanaualhaque@gmail.com>
2024-02-14 12:09:54 +0600

    2nd commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const submitBtn = document.getElementById("submit");
+
+
+const btns = document.querySelectorAll("a");
+
+
+
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
